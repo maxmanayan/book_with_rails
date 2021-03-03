b08cd69dab13aeaa46663ac206cbcac6f321d688
Max Manayan <maximillian521@gmail.com>
2021-03-03 15:01:22 -0700

    model,controller, and api created and db seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require "faker"
+
+Book.destroy_all
+
+
+10.times do
+  Book.create(
+    title: Faker::DcComics.title,
+    author: Faker::DcComics.name,
+  )
+end
+
+
+puts "Seeded #{Book.all.size} new books"
